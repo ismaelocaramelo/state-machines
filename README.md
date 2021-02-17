c295b3766c731c96b5befd2d7ee681f4f2636c45
Ismael Bakkali <ismael.bakkali@guidesmiths.com>
2021-02-17 10:18:19 +0100

    Add exercise 03

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 ## [Theory](./docs/state-machine)
 
+## Get started
+
+`npm start`
+
+Go to `http://localhost:1234`
+
+And then you can go to any exercise like this:
+
+http://localhost:1234/03/index.html
+
 ## Tasks
 
 - [Counter](./tasks/counter/counterMachine.md)
---
+++ exercises/03/index.js
@@ -0,0 +1,45 @@
+import { createMachine, interpret } from "xstate";
+
+const elBox = document.querySelector("#box");
+
+const machine = createMachine({
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: {
+        MOUSEDOWN: "active",
+      },
+    },
+    active: {
+      on: {
+        MOUSEUP: "inactive",
+      },
+    },
+  },
+});
+
+// Create a service using interpret(...)
+const service = interpret(machine).onTransition((state) => {
+  elBox.dataset.state = state.value;
+});
+
+// Listen to state transitions and set
+// `elBox.dataset.state` to the state value as before.
+// ...
+
+// Start the service.
+// ...
+
+service.start();
+
+elBox.addEventListener("mousedown", (event) => {
+  // Send a mousedown event
+  // ...
+  service.send("MOUSEDOWN");
+});
+
+elBox.addEventListener("mouseup", (event) => {
+  // Send a mouseup event
+  // ...
+  service.send("MOUSEUP");
+});
